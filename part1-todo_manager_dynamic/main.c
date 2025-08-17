1cdc2d99b8cee7c73853ca936460002b1a38c983
autoselff <60112867+autoselff@users.noreply.github.com>
2025-08-17 15:01:24 +0200

    Update main.c

--- part1-todo_manager_dynamic/main.c
+++ part1-todo_manager_dynamic/main.c
@@ -8,55 +8,55 @@ int main(){
     char actions[16][32] = {
         "Prototype Action"
     };
-
+    
     int actions_len = sizeof(actions) / sizeof(actions[0]);
 
     int choice;
     
     while (choice != 4) {
     
-    printf("Podaj akcje\n");
-    scanf("%d", &choice); 
-
-    switch(choice) {
-        case 1:
-            char a[32];
-            printf("Podaj akcje do wpisania\n");
-            scanf("%s", &a);
-            a[strcspn(a, "\n")] = '\0';
-
-            int index;
-            printf("Podaj index nowej akcji\n");
-            scanf("%d", &index);
-            getchar();
-
-            strcpy(actions[index], a);
-            printf("Poprawnie zapisano akcje...\n");
-            break;
-
-        case 2:
-            int index_to_remove;
-            printf("Podaj index aktywnosci do usuniecia\n");
-            scanf("%d", &index_to_remove);
-            getchar();
-
-            actions[index_to_remove][0] = '\0';
-            printf("Poprawnie usunieto akcje\n");
-            break;
-
-        case 3:
-            for(int i = 0; i < actions_len; ++i) {
-                if (actions[i][0] != '\0') printf("%d - %s\n", i, actions[i]);
-            }
-            break;
-
-        case 4:
-            printf("Koniec programu.\n");
-            break;
-
-        default:
-            printf("Niepoprawna akcja.\n");
-            break;
-    }       
-}
+        printf("Podaj akcje\n");
+        scanf("%d", &choice); 
+    
+        switch(choice) {
+            case 1:
+                char a[32];
+                printf("Podaj akcje do wpisania\n");
+                scanf("%s", &a);
+                a[strcspn(a, "\n")] = '\0';
+    
+                int index;
+                printf("Podaj index nowej akcji\n");
+                scanf("%d", &index);
+                getchar();
+    
+                strcpy(actions[index], a);
+                printf("Poprawnie zapisano akcje...\n");
+                break;
+    
+            case 2:
+                int index_to_remove;
+                printf("Podaj index aktywnosci do usuniecia\n");
+                scanf("%d", &index_to_remove);
+                getchar();
+    
+                actions[index_to_remove][0] = '\0';
+                printf("Poprawnie usunieto akcje\n");
+                break;
+    
+            case 3:
+                for(int i = 0; i < actions_len; ++i) {
+                    if (actions[i][0] != '\0') printf("%d - %s\n", i, actions[i]);
+                }
+                break;
+    
+            case 4:
+                printf("Koniec programu.\n");
+                break;
+    
+            default:
+                printf("Niepoprawna akcja.\n");
+                break;
+        }       
+    }
 }
